87b95924f7ecff33be24d39136cd14fca48bc29b
maartenwn <maartenwn@gmail.com>
2020-12-10 00:06:20 +0100

    added coutUp script

---
+++ countUp.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+startDate="$(date +%s.%N)" 
+while true; do
+current="$(date +%s.%N)" 
+diff=$(echo "$current-$startDate" |bc)
+time="$(date +%T -d @0$diff)" #Pad diff with leading zero
+realTime="$(date +%T -d "$time + 3 hour")"
+echo $realTime
+echo $realTime > test.txt
+
+done
